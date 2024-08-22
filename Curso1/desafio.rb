355013272a69b06faa3d414c1fe2adc1c1a88083
lipebarba2 <felipecinemkt@gmail.com>
2024-08-22 15:18:16 -0300

    exercicios

---
+++ Curso1/desafio.rb
@@ -0,0 +1,8 @@
+puts('Digite o seu nome:')
+nome = gets.chomp
+puts('Digite seu sobrenome')
+sobrenome = gets.chomp
+puts('Digite a sua idade')
+idade = gets.chomp
+
+puts("Seu nome é #{nome} #{sobrenome}, e sua idade é #{idade} anos")
